cd358ad261438b2121384b02f6d0f90e959f4bdb
Bhanu Vardhan <bhanuvardhan.tata@gmail.com>
2025-03-31 12:27:36 +0530

    Primary React-app

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+function Button(){
+    return (
+        <div>
+            <button type="button" className="btn-cls">
+                Shanvith
+            </button>
+        </div>
+    );
+}
+export default Button;
